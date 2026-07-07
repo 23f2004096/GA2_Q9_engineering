d013de62242ebc719f35cd9ba7bf6d14ef73ff2a
Devi Saranya <23f2004096@ds.study.iitm.ac.in>
2026-07-07 15:52:15 +0400

    update in app

--- app.py
+++ app.py
@@ -10,9 +10,14 @@ app = FastAPI()
 # -----------------------------
 # Enable CORS
 # -----------------------------
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
